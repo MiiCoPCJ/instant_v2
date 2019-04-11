8c04f04a6ee4840fc0aedb962d4b7d24d6953ecf
MiiCoPCJ <EngMoreSite@outlook.com>
2019-04-11 17:02:38 +0800

    初模板

---
+++ env_example.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+      'host'=>'127.0.0.1',
+      'port'=>3306,
+      'dbname'=>'instant',
+      'username'=>'root',
+      'pwd'=>'123'
+    ];
+
+?>
---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+$config = require_once(__DIR__.'/env.php');
+define('CONFIG',$config);
+require_once(__DIR__.'/common/Base.php');
