e98e2044c25da5125dcf5df4b5ff199d94ab479b
MiiCoPCJ <EngMoreSite@outlook.com>
2019-04-12 14:14:38 +0800

    添加删除数据函数

---
+++ controllers/CleanData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace controllers;
+require_once(__dir__.'/../init.php');
+use common\Controller;
+
+
+
+class CleanData extends Controller
+{
+
+    //获取okex的Api数据
+    public function Clean()
+    {
+      $time = time();
+      $sql = "delete from `eos` where time<".($time-3600);
+      $result = $this->db->Execute($sql);
+      $msg = "time: ".date('Y-m-d H:i:s',$time).' '.$sql;
+      $this->func->log_file($msg,true,'delete_data.log');
+    }
+
+
+}
+
+$clean = new CleanData();
+$clean->Clean();
